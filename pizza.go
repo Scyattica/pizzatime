80163161721177a43a7a1a418d34982a265ba426
Scy <scyattica17@gmail.com>
2022-05-12 18:00:37 -0400

    new

--- pizza.go
+++ pizza.go
@@ -10,50 +10,50 @@ import (
 )
 
 type loljson struct {
-	data data `json:"data"`
-	meta meta   `json:"meta"`
+	Data []data `json:"data"`
+	Meta meta   `json:"meta"`
 }
 
 type data struct {
-	id                 string
-	date               string
-	home_team          teams
-	home_team_score    int
-	period             int
-	postseason         bool
-	season             int
-	status             string
-	time               string
-	vistor_team        teams
-	visitor_team_score int
+	Id                 int    `json:"id"`
+	Date               string `json:"date"`
+	Home_team          teams  `json:"home_team"`
+	Home_team_score    int    `json:"home_team_score"`
+	Period             int    `json:"period"`
+	Postseason         bool   `json:"postseason"`
+	Season             int    `json:"season"`
+	Status             string `json:"status"`
+	Time               string `json:"time"`
+	Vistor_team        teams  `json:"visitor_team"`
+	Visitor_team_score int    `json:"visitor_team_score"`
 }
 
 type meta struct {
-	total_pages  int
-	current_page int
-	next_page    int
-	per_page     int
-	total_count  int
+	Total_pages  int
+	Current_page int
+	Next_page    int
+	Per_page     int
+	Total_count  int
 }
 
 type teams struct {
-	id           int
-	abbreviation string
-	city         string
-	conference   string
-	division     string
-	full_name    string
-	name         string
+	Id           int `json:"id"`
+	Abbreviation string
+	City         string
+	Conference   string
+	Division     string
+	Full_name    string
+	Name         string
 }
 
 func pizzaurl() (pizzaurl string) {
-	year, month, day := time.Now().Date()
-	return fmt.Sprintf("https://www.balldontlie.io/api/v1/games?seasons[]=%d&team_ids[]=16&dates[]=%d-%d-%d", year-1, year, int(month), day)
+	time := time.Now()
+	return fmt.Sprintf("https://www.balldontlie.io/api/v1/games?team_ids[]=16&dates[]=%d-%d-%d", time.Year(), time.Month(), time.AddDate(0, 0, -1).Day())
 }
 
-func getresult() (result string) {
+func getresult() (result *loljson) {
 	url := pizzaurl()
-	println(url)
+	//fmt.Printf(url)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
@@ -62,14 +62,31 @@ func getresult() (result string) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	println(string(body))
 	jr := &loljson{}
 	json.Unmarshal(body, &jr)
-	fmt.Println(jr)
-	return "lol"
+	return jr
 }
 
 func main() {
-	println("Hi")
-	getresult()
+	parseresults(getresult())
+}
+
+func parseresults(uj *loljson) (pizzatime bool) {
+	if uj.Meta.Total_count != 1 {
+		fmt.Printf("I don't think the heat played yesterday.")
+		return false // heat didn't play.
+	} else {
+		if uj.Data[0].Home_team.Name == "Heat" {
+			if uj.Data[0].Home_team_score > uj.Data[0].Visitor_team_score {
+				fmt.Println("Heat won! probably should buy pizza.")
+			}
+		} else if uj.Data[0].Vistor_team.Name == "Heat" {
+			if uj.Data[0].Visitor_team_score > uj.Data[0].Home_team_score {
+				fmt.Println("Heat won! probably should buy pizza.")
+			}
+		} else {
+			fmt.Println("Heat didn't play/something else happened. ")
+		}
+	}
+	return false
 }
